9ba2ebf50a39795e15991dd72ea5ccd514744ccf
FeriVOQ <36544651+ferivoq@users.noreply.github.com>
2025-10-27 11:49:54 +0100

    Update issue URL for GitHub repository

--- src/constants/utils.ts
+++ src/constants/utils.ts
@@ -1,5 +1,5 @@
 export const SPONSOR_URL = 'https://ko-fi.com/aleganza';
 export const OPEN_NEW_ISSUE_URL =
-  'https://github.com/aleganza/akuse/issues/new';
+  'https://github.com/waruhachi/akuse/issues/new';
 export const EPISODES_INFO_URL = 'https://api.ani.zip/mappings?anilist_id=';
 export const SOFAMAXXING_URL = 'https://sofamaxxing.vercel.app';
